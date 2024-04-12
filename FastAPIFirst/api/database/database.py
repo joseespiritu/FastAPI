df0ddaeeab1e160552ab6c9a50daa64f006810d6
joseespiritu <joseespiritu89@aragon.unam.mx>
2024-04-11 21:08:09 -0600

    RDS Connection MySQL

--- FastAPIFirst/api/database/database.py
+++ FastAPIFirst/api/database/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./fastapi.db"
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./fastapi.db"
+SQLALCHEMY_DATABASE_URL = "mysql://admin:password@rdsendpoint/fastapi"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
